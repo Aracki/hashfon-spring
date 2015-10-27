8835c6420a5da2671734bef49afc384589063cc7
Aracki <aracki.ivan@gmail.com>
2015-10-27 13:10:30 +0100

    Repaired MvcConfig paths.. And fixed sidebar in s_style.css

--- src/main/java/com/ro/config/MvcConfig.java
+++ src/main/java/com/ro/config/MvcConfig.java
@@ -14,8 +14,15 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
         registry.addViewController("/error").setViewName("error");
-        registry.addViewController("/pocetna").setViewName("pocetna");
-        registry.addViewController("/student").setViewName("student");
-        registry.addViewController("/kompanija_profil").setViewName("kompanija_profil");
+
+        //KLIJENT - KOMPANIJA
+        registry.addViewController("/k_pocetna").setViewName("k_pocetna");
+        registry.addViewController("/k_student_profil").setViewName("k_student_profil");
+        registry.addViewController("/k_kompanija_profil").setViewName("k_kompanija_profil");
+
+        //KLIJENT - STUDENT
+        registry.addViewController("/s_pocetna").setViewName("s_pocetna");
+        registry.addViewController("/s_student_profil").setViewName("s_student_profil");
+        registry.addViewController("/s_kompanija_profil").setViewName("s_kompanija_profil");
     }
 }
